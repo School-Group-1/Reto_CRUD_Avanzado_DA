c81ce04ae891932e3197d17a812c7e60f79f5844
kevvo23 <kssalamea@gmail.com>
2026-02-02 10:38:49 +0100

    ProfileWindow JasperReport

--- src/report/ReportService.java
+++ src/report/ReportService.java
@@ -1,10 +1,12 @@
 package report;
 
 import java.io.InputStream;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 
 import model.Company;
+import model.Profile;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.view.JasperViewer;
@@ -25,6 +27,37 @@ public class ReportService {
             // 3. Pasar los datos
             JRBeanCollectionDataSource dataSource =
                     new JRBeanCollectionDataSource(companies);
+
+            // 4. Rellenar el reporte
+            JasperPrint jasperPrint =
+                    JasperFillManager.fillReport(
+                            jasperReport,
+                            new HashMap<>(),
+                            dataSource
+                    );
+
+            // 5. Mostrar el PDF
+            JasperViewer.viewReport(jasperPrint, false);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void generateUserReport(Profile profile) {
+
+        try {
+            // 1. Cargar el diseño jrxml
+            InputStream reportStream = getClass()
+                    .getResourceAsStream("/report/profile_report.jrxml");
+
+            // 2. Compilar el reporte
+            JasperReport jasperReport =
+                    JasperCompileManager.compileReport(reportStream);
+
+            // 3. Pasar los datos
+            JRBeanCollectionDataSource dataSource =
+                    new JRBeanCollectionDataSource(java.util.Collections.singletonList(profile));
 
             // 4. Rellenar el reporte
             JasperPrint jasperPrint =
